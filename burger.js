4475c3129731a1499159a0336e5441787a37490e
giorgivawadze <116506482+giorgivawadze@users.noreply.github.com>
2023-04-17 20:21:58 +0400

    update###

--- burger.js
+++ burger.js
@@ -1,46 +1,259 @@
-
-const menuBtn  = document.querySelector('.burger');
+const menuBtn = document.querySelector(".burger");
 let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-   if(!menuOpen) {
-    menuBtn.classList.add('open');
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
     menuOpen = true;
-   } else {
-    menuBtn.classList.remove('open');
+  } else {
+    menuBtn.classList.remove("open");
     menuOpen = false;
-   }
-   
-
+  }
 });
-const toggleButton = document.querySelector ('.burger')
-const navbarLinks = document.querySelector ('.ultag')
 
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle ('active')
-})
+// Fix: Remove duplicate const declaration for toggleButton
+const toggleButton = document.querySelector(".burger");
+const navbarLinks = document.querySelector(".ultag");
+// const toggleBtn = document.querySelector('.sidebar');
+// const mainDiv = document.querySelector('.header');
+
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+}); // Fix: Add missing closing parenthesis
+
+// toggleBtn.addEventListener('click', () => {
+//   mainDiv.classList.toggle('active');
+// }); // Fix: Remove extra closing parenthesis
+
 
 
 
 
 // darktheme
+function myFunction3() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
 
-   function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-    
-    // Check if the user has set a theme preference before
-    if (localStorage.getItem("theme") === "dark") {
-      // If the preference was set to dark, remove it
-      localStorage.removeItem("theme");
-    } else {
-      // Otherwise, set the preference to dark
-      localStorage.setItem("theme", "dark");
-    }
-  }
-  
-  // When the page loads, check if the user has set a theme preference before
+  // Check if the user has set a theme preference before
   if (localStorage.getItem("theme") === "dark") {
-    // If the preference was set to dark, enable the dark mode
-    document.body.classList.add("dark-mode");
+    // If the preference was set to dark, remove it
+    localStorage.removeItem("theme");
+  } else {
+    // Otherwise, set the preference to dark
+    localStorage.setItem("theme", "dark");
   }
-  
+}
+
+// When the page loads, check if the user has set a theme preference before
+if (localStorage.getItem("theme") === "dark") {
+  // If the preference was set to dark, enable the dark mode
+  document.body.classList.add("dark-mode");
+}
+// document ready handler
+// or $(document).ready(Function(){...
+jQuery(function ($) {
+  var checkboxValue = JSON.parse(localStorage.getItem("checkboxValue")) || {};
+  var $checkbox = $("#checkbox-container :checkbox");
+
+  $checkbox.on("change", function () {
+    $checkbox.each(function () {
+      checkboxValue[this.id] = this.checked;
+    });
+    localStorage.setItem("checkboxValue", JSON.stringify(checkboxValue));
+  });
+
+  //on page load
+  $.each(checkboxValue, function (key, value) {
+    $("#" + key).prop("checked", value);
+  });
+});
+const paginationNumbers = document.getElementById("pagination-numbers");
+const paginatedList = document.getElementById("paginated-list");
+const listItems = paginatedList.querySelectorAll("li");
+const nextButton = document.getElementById("next-button");
+const prevButton = document.getElementById("prev-button");
+
+const paginationLimit = 6;
+const pageCount = Math.ceil(listItems.length / paginationLimit);
+let currentPage = 1;
+
+const disableButton = (button) => {
+  button.classList.add("disabled");
+  button.setAttribute("disabled", true);
+};
+
+const enableButton = (button) => {
+  button.classList.remove("disabled");
+  button.removeAttribute("disabled");
+};
+
+const handlePageButtonsStatus = () => {
+  if (currentPage === 1) {
+    disableButton(prevButton);
+  } else {
+    enableButton(prevButton);
+  }
+
+  if (pageCount === currentPage) {
+    disableButton(nextButton);
+  } else {
+    enableButton(nextButton);
+  }
+};
+
+const handleActivePageNumber = () => {
+  document.querySelectorAll(".pagination-number").forEach((button) => {
+    button.classList.remove("active");
+    const pageIndex = Number(button.getAttribute("page-index"));
+    if (pageIndex == currentPage) {
+      button.classList.add("active");
+    }
+  });
+};
+
+const appendPageNumber = (index) => {
+  const pageNumber = document.createElement("button");
+  pageNumber.className = "pagination-number";
+  pageNumber.innerHTML = index;
+  pageNumber.setAttribute("page-index", index);
+  pageNumber.setAttribute("aria-label", "Page " + index);
+
+  paginationNumbers.appendChild(pageNumber);
+};
+
+const getPaginationNumbers = () => {
+  for (let i = 1; i <= pageCount; i++) {
+    appendPageNumber(i);
+  }
+};
+
+const setCurrentPage = (pageNum) => {
+  currentPage = pageNum;
+
+  handleActivePageNumber();
+  handlePageButtonsStatus();
+
+  const prevRange = (pageNum - 1) * paginationLimit;
+  const currRange = pageNum * paginationLimit;
+
+  listItems.forEach((item, index) => {
+    item.classList.add("hidden");
+    if (index >= prevRange && index < currRange) {
+      item.classList.remove("hidden");
+    }
+  });
+};
+
+window.addEventListener("load", () => {
+  getPaginationNumbers();
+  setCurrentPage(1);
+
+  prevButton.addEventListener("click", () => {
+    setCurrentPage(currentPage - 1);
+  });
+
+  nextButton.addEventListener("click", () => {
+    setCurrentPage(currentPage + 1);
+  });
+
+  document.querySelectorAll(".pagination-number").forEach((button) => {
+    const pageIndex = Number(button.getAttribute("page-index"));
+
+    if (pageIndex) {
+      button.addEventListener("click", () => {
+        setCurrentPage(pageIndex);
+      });
+    }
+  });
+});
+
+function togglePopup() {
+  document.getElementById("popup-1").classList.toggle("active");
+}
+
+function togglePopup2() {
+  document.getElementById("popup-2").classList.toggle("active");
+}
+
+function togglePopup3() {
+  document.getElementById("popup-2").classList.toggle("active");
+}
+
+function togglePopup4() {
+  document.getElementById("popup-4").classList.toggle("active");
+}
+
+function togglePopup5() {
+  document.getElementById("popup-5").classList.toggle("active");
+}
+
+function togglePopup6() {
+  document.getElementById("popup-6").classList.toggle("active");
+}
+
+function togglePopup7() {
+  document.getElementById("popup-7").classList.toggle("active");
+}
+
+function togglePopup8() {
+  document.getElementById("popup-8").classList.toggle("active");
+}
+
+function togglePopup9() {
+  document.getElementById("popup-9").classList.toggle("active");
+}
+
+function togglePopup10() {
+  document.getElementById("popup-10").classList.toggle("active");
+}
+
+function togglePopup11() {
+  document.getElementById("popup-11").classList.toggle("active");
+}
+function togglePopup12() {
+  document.getElementById("popup-12").classList.toggle("active");
+}
+
+// gallery
+
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+
+// gallery end
--- index.js
+++ index.js
@@ -1,23 +1,95 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   autoplay: {
-    delay :3000,
+    delay: 3000,
     disableOnInteraction: false,
   },
   loop: true,
 
   // If we need pagination
   pagination: {
-    el: '.paginations',
-    clickable : true,
+    el: ".paginations",
+    clickable: true,
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiperbtn2",
+    prevEl: ".swiperbtn1",
   },
   // And if we need scrollbar
   scrollbar: {
-    el: '.swiper-scrollbar',
+    el: ".swiper-scrollbar",
   },
-  
 });
 
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction10() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+function myFunction2() {
+  document.getElementById("myDropdown2").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn2")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content2");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+function myFunction4() {
+  document.getElementById("myDropdown3").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn3")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content3");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+function myFunction5() {
+  document.getElementById("myDropdown4").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn4")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content4");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
